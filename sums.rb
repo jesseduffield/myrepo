b87b95e70dfa0b82b067737cdd4124f5fe33c432
Jesse Duffield <jessedduffield@gmail.com>
2019-01-31 20:47:46 +1100

    add more tables

---
+++ sums.rb
@@ -0,0 +1,14 @@
+puts 'here we go!'
+puts 'how hard do you want this to be?'
+max = gets.to_i
+(0..10).each do
+  i1 = rand(max)
+  i2 = rand(max)
+  puts "#{i1} + #{i2}"
+  answer = gets
+  if answer.to_i == i1 + i2
+    puts 'correct'
+  else
+    puts "incorrect: answer is #{i1 + i2}"
+  end
+end
